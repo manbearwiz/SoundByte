91a9aa135489f168209f97aa45b462f73e122485
Kevin Brey <breyk@uwplatt.edu>
2014-07-06 15:00:49 -0500

    Add download code

--- SoundByte.py
+++ SoundByte.py
@@ -1,17 +1,26 @@
 import soundcloud
-
+import re
+import urllib
 CLIENT_ID = 'f06362a90de4a12555c9798e8a9b27d3'
-CLIENT_SECRET = 'ce4dbed1c6255b897c6bb7c5401d2c68'
-USERNAME = 'user'
-PASSWORD = 'pass'
 
-client = soundcloud.Client(
-	client_id=CLIENT_ID,
-	client_secret=CLIENT_SECRET,
-	username=USERNAME,
-	password=PASSWORD
-)
+client = soundcloud.Client(client_id=CLIENT_ID)
 
 
-print(client.get('/me'))
+user = client.get('/resolve', url='https://soundcloud.com/kevin-brey')
 
+print(user.id)
+
+favorite_tracks = client.get('/users/{0}/favorites'.format(user.id), limit=10)
+
+for f in favorite_tracks:
+    match = re.match('https://[^/]*/(.*)_m.png', f.waveform_url)
+    
+    id = match.group(1)
+
+    sound_url = 'http://media.soundcloud.com/stream/{0}'.format(id)
+
+    urllib.request.urlretrieve(sound_url, '{0}.mp3'.format(id))
+
+    print(match.group(1))
+
+    print(f.title + " " + f.waveform_url)
