e8d4aab88b72a0b49a9f5d00fd40162467fcb09a
Kevin Brey <breyk@uwplatt.edu>
2014-07-06 12:09:21 -0500

    Attempt to access user data

--- SoundByte.py
+++ SoundByte.py
@@ -1,5 +1,17 @@
 import soundcloud
 
-CLIENT_ID = f06362a90de4a12555c9798e8a9b27d3
+CLIENT_ID = 'f06362a90de4a12555c9798e8a9b27d3'
+CLIENT_SECRET = 'ce4dbed1c6255b897c6bb7c5401d2c68'
+USERNAME = 'user'
+PASSWORD = 'pass'
 
-client = soundcloud.Client(client_id=CLIENT_ID)
+client = soundcloud.Client(
+	client_id=CLIENT_ID,
+	client_secret=CLIENT_SECRET,
+	username=USERNAME,
+	password=PASSWORD
+)
+
+
+print(client.get('/me'))
+
