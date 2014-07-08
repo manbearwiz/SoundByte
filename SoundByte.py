3f54c72c8624e894c4e9f68c1f4cd731d3d33f7a
kmb32123 <breyk@uwplatt.edu>
2014-07-08 00:34:24 -0500

    Add record file
    Keep record of tracks that have been downloaded.
    Allows me to start/stop/move files and never worry about downloading
    twice.

--- SoundByte.py
+++ SoundByte.py
@@ -1,28 +1,52 @@
 import soundcloud
 import re
 import urllib
+
+from urllib.error import HTTPError
+
 CLIENT_ID = 'f06362a90de4a12555c9798e8a9b27d3'
+
+RECORD_FILE = 'soundbyte_record.txt'
+
+USERNAME = 'kevin-brey'
 
 client = soundcloud.Client(client_id=CLIENT_ID)
 
 
-user = client.get('/resolve', url='https://soundcloud.com/kevin-brey')
+user = client.get('/resolve', url='https://soundcloud.com/{username}'.format(username=USERNAME))
 
-print(user.id)
+with open(RECORD_FILE) as f:
+   already_downloaded = f.readlines()
+   
+already_downloaded = set([int(x.rstrip()) for x in already_downloaded])
 
-favorite_tracks = client.get('/users/{0}/favorites'.format(user.id), limit=10)
+favorite_tracks = client.get('/users/{0}/favorites'.format(user.id), limit=300)
 
-for f in favorite_tracks:
-    match = re.match('https://[^/]*/(.*)_m.png', f.waveform_url)
+tracks_to_download = [x for x in favorite_tracks if x.id not in already_downloaded]
+
+print("Found {numtracks} favorite tracks you have yet to download.".format(numtracks=len(tracks_to_download)))
+
+with open(RECORD_FILE, 'a') as output_file:
+    for f in tracks_to_download:
+        match = re.match('https://[^/]*/(.*)_m.png', f.waveform_url)
+        
+        id = match.group(1)
     
-    id = match.group(1)
-
-    sound_url = 'http://media.soundcloud.com/stream/{0}'.format(id)
-
-    filename = '{artist} - {title}.mp3'.format(artist=f.user['username'], title=f.title)
-
-    print('Downloading "{filename}" from "{url}"...'.format(filename=filename, url=sound_url))
-
-    urllib.request.urlretrieve(sound_url, filename)
-
-    print('Download complete!')
+        sound_url = 'http://media.soundcloud.com/stream/{0}'.format(id)
+	
+        artist = f.user['username']
+    	
+        filename = '{artist} - {title}.mp3'.format(artist=artist, title=f.title)
+    
+        filename = re.sub('[^\w\-_\. \(\)\'\[\]\&\#]', ' - ', filename)
+		
+        filename = re.sub(' +',' ', filename)
+				
+        print('Downloading "{filename}" from "{url}"...'.format(filename=filename, url=sound_url))
+    
+        try:
+            urllib.request.urlretrieve(sound_url, filename)
+            print('Download complete!')
+            output_file.write('{song_id}\n'.format(song_id=f.id))
+        except HTTPError:
+            print("I couldn't download the file ({url}). Now I'm sad.".format(url=sound_url))
